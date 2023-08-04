32ab1ee56e8dfa0439579ba635f94b8adc3e93ea
Verifryd <verifryd.video@gmail.com>
2023-08-04 13:37:25 -0400

    fkdj

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development', // or 'production'
-  entry: './Eggsperience/src/script.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    mode: 'development', // or 'production'
+    entry: './Eggsperience/src/script.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
